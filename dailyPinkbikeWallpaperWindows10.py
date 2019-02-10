62eef17fdcc080d83ae99252bd7a9a4e9571cf32
nakami <nakami@users.noreply.github.com>
2019-02-10 19:23:53 +0100

    Update dailyPinkbikeWallpaperWindows10.py

--- dailyPinkbikeWallpaperWindows10.py
+++ dailyPinkbikeWallpaperWindows10.py
@@ -7,7 +7,7 @@ import ctypes
 import sys
 
 
-r = requests.get("https://www.pinkbike.com/")
+r = requests.get("https://www.pinkbike.com/photo/podlist/")
 data = r.text
 soup = BeautifulSoup(data, "lxml")
 
